a9522352a007e688db096977fc2e50cf73db6378
jameskdc <jamesjl24@gmail.com>
2026-06-29 15:07:01 +0000

    Adicionei 2 cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,40 @@
 
 
 int main() {
+  char estado;
+  char codigodacarta[3];
+  char nomedacidade[30];
+  int populacao;
+  float areaemkm;
+  float pib;
+  int numerodepontosturisticos;
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  printf("Oi boboca!\n");
+  // Carta 1
+  printf("Carta 1\n");
+  printf("Estado: A\n", estado);
+  printf("Código: A01\n", codigodacarta);
+  printf("Nome da Cidade: São Paulo\n", nomedacidade);
+  printf("População: 458712564\n", populacao);
+  printf("Área em KM²: 1021.65\n", areaemkm);
+  printf("PIB: 699.28 Bilhões de reais\n", pib);
+  printf("Número de Pontos Turísticos: 32\n", numerodepontosturisticos);
+
+  // Pula Linha separa Cartas
+  printf("\n");
+
+  // Carta 2
+  printf("Carta 2\n");
+  printf("Estado: B\n", estado);
+  printf("Código: B02\n", codigodacarta);
+  printf("Nome da Cidade: Rio de Janeiro\n", nomedacidade);
+  printf("População: 25468972\n", populacao);
+  printf("Área em KM²: 2021.11\n", areaemkm);
+  printf("PIB: 800.78 Bilhões de reais\n", pib);
+  printf("Número de Pontos Turísticos: 56\n", numerodepontosturisticos);
+
+
+
 
   // Área para entrada de dados
 
