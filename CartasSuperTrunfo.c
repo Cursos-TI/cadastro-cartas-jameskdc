05216636db3ce934c055c979830c13af9bfe9389
jameskdc <jamesjl24@gmail.com>
2026-06-29 13:44:40 +0000

    Inicial

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,8 +4,10 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  printf("Oi boboca!\n")
 
   // Área para entrada de dados
 
